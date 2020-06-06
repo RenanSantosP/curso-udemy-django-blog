79ae963044d368bfe0b4498d6719eec18561135d
Renan Santos <renan3santos@gmail.com>
2020-06-06 11:52:01 -0300

    Organização de Pastas e Css

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='publicado')
+
 class Post(models.Model):
     STATUS = (
         ('rascunho', 'Rascunho'),
@@ -16,8 +21,11 @@ class Post(models.Model):
     alterado = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS, default='rascunho')
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
-        ordering = ('publicado',)
+        ordering = ('-publicado',)
 
     def __str__(self):
         return self.titulo
--- meu_site/settings.py
+++ meu_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'meu_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,16 +106,22 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIIA_ROOT = 'media'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    'statics'
+]
